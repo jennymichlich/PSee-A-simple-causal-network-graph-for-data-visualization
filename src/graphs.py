e36348ba2e79fa5602560db24c25df8bf81bf1bf
Carlo Tak <carlotak@asu.edu>
2026-02-04 16:29:37 +0200

    Created skeleton for graph visualization tools (issue #5).

---
+++ src/graphs.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+def draw_causal_graph(graph, title="Causal Graph", node_color='lightblue'):
+    """
+    Addresses Issue #5: Drawing the picture.
+    
+    This takes our two-variable graph and draws it nicely. We'll make sure
+    the nodes look clean and the arrow (if there is one) is clear.
+    """
+    # TODO: Set up the figure size
+    # TODO: Draw the two nodes
+    # TODO: Draw the edge connecting them
+    # TODO: Add the labels
+    pass
+
+def save_graph_to_file(graph, filename):
+    """
+    Addresses Issue #5: Saving the file.
+    
+    Saves the picture we just drew as a PNG or JPEG so we can put it 
+    in a report.
+    """
+    # TODO: Save the figure to the specific filename
+    pass
