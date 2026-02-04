bff54081306a12f96458bb5d756a4170070d10d5
Carlo Tak <carlotak@asu.edu>
2026-02-04 16:16:26 +0200

    Create interface for loading Tuebingen data pairs (issue #2).

---
+++ src/loaders.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def load_tuebingen_pair(folder_path, file_name):
+    """
+    Grabs one of those specific pair files from the Tuebingen folder.
+    
+    This handles Issue #2. We are focused on just getting a two-variable 
+    system loaded up so the algorithm has something to chew on.
+    """
+    # TODO: Find the specific text file in the folder
+    # TODO: Load the two columns of data (Variable A and Variable B)
+    # TODO: Return the data table
+    pass
